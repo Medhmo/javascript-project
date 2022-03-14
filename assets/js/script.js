f69d0cbfbffd9ddf7314f122101c6c7bf1d99320
Medhmo <chrisconde18@gmail.com>
2022-03-14 22:09:27 +0000

    creates  the basic javascript structure

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const playerscore = document.getElementById("player-score");
+
+const playerhand = document.getElementById("player-hand");
+
+const computerscore = document.getElementById("computer-score");
+const computerhand = document.getElementById("computer-hand");
+
+const playerrock = document.getElementById("player-rock");
+const playerpaper = document.getElementById("player-paper");
+const playerscissor = document.getElementById("player-scissor");
+
+const computerrock = document.getElementById("computer-rock");
+const computerpaper = document.getElementById("computer-paper");
+const computerscissor = document.getElementById("computer-scissor");
+
+const gameresult = document.getElementById("game-result");
+
+const gameimages = document.querySelectorAll("img");
